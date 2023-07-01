8ed38c636a95d9448361d838b8efbb74a01ddb8c
bmancilla <bmancilla119@gmail.com>
2023-07-01 15:44:34 -0500

    style: Componente Navbar
    Componente Navbar implementado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import './styles/App.css'
 import { Home } from './pages/home/Home'
+import { Navbar } from './components/navbar/Navbar'
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
       </Routes>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../../assets/logo.png'
+import './navbar.css'
+
+const Navbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const closeMenu = () => {
+        setIsOpen(false)
+    }
+
+  return (
+    <header className='header'>
+        <nav className='header__nav'>
+            <div className="header__logo">
+                <img src={logo} className='header__logo-icono' alt="" />
+                <p className="header__logo-titulo">Fitfuel</p>
+            </div>
+            <ul className={`header__menu ${isOpen && 'open'}`}>
+                <li className="header__menu-element" onClick={closeMenu}>
+                    <NavLink to="/" className="header__menu-link">Home</NavLink>
+                </li>
+                <li className="header__menu-element" onClick={closeMenu}>
+                    <NavLink to="/" className="header__menu-link">Service</NavLink>
+                </li>
+                <li className="header__menu-element" onClick={closeMenu}>
+                    <NavLink to="/" className="header__menu-link">About Us</NavLink>
+                </li>
+                <li className="header__menu-element" onClick={closeMenu}>
+                    <NavLink to="/" className="header__menu-link">Contact</NavLink>
+                </li>
+            </ul>
+            <div className={`header__toogle ${isOpen && 'open'}`} onClick={()=>{setIsOpen(!isOpen)}}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </nav>
+    </header>
+
+  )
+}
+
+export {Navbar};
