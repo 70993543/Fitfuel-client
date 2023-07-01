7efabfa2edb4d1045fdd41428b0a26377ce24fe9
bmancilla <bmancilla119@gmail.com>
2023-07-01 10:45:51 -0500

    feat: Implementación de React-doom
    Instalación de dependencias y configuraciones generales

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import './styles/App.css'
+import { Home } from './pages/home/Home'
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
