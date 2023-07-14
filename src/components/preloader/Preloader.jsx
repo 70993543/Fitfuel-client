50a1f670c2caa7f9aeeff23f609304128e17a43f
bmancilla <bmancilla119@gmail.com>
2023-07-14 15:05:04 -0500

    feat: Preloader implementado
    Preloader implementado para la pagina inicio, servicio y nosotros

---
+++ src/components/preloader/Preloader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './preloader.css'
+
+const Preloader = () => {
+  return (
+    <div className="preloader-container">
+        <div className="preloader-spinner"></div>
+    </div>
+  )
+}
+
+export {Preloader}
